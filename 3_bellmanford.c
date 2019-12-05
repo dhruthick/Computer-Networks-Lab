bb6bf70b4e857494c3b710917a37de740e345d4a
Dhruthick Gowda M <54900021+dhruthick@users.noreply.github.com>
2019-12-05 20:52:23 +0530

    works for negative weights

--- 3_bellmanford.c
+++ 3_bellmanford.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int distance[100],next[100],cost[100][100],src,flag,n,i,j;
+int distance[100],next[100],cost[100][100],src,flag,n,i,j,k;
 void printpath(int v)
 {
     if(next[v]==-1)
@@ -19,25 +19,30 @@ void main(){
     for(i=1;i<=n;i++)
         for(j=1;j<=n;j++)
             scanf("%d",&cost[i][j]);
-    for(src=1;src<=n;src++){
+    //for(src=1;src<=n;src++){
         for(i=1;i<=n;i++){
             distance[i]=999;
             next[i]=-1;
         }
-        //printf("Enter the source node: ");
-        //scanf("%d",&src);
+        printf("Enter the source node: ");
+        scanf("%d",&src);
         distance[src]=0;
-        do{
-            flag=0;
+        for(k=1;k<n;k++){
             for(i=1;i<=n;i++)
                 for(j=1;j<=n;j++){
-                    if(cost[i][j]!=999 && distance[i]>distance[j]+cost[i][j]){
-                        distance[i]=distance[j]+cost[i][j];
-                        next[i]=j;
-                        flag=1;
+                    if(cost[i][j]!=999 && cost[i][j]!=0 && distance[j]>distance[i]+cost[i][j]){
+                        distance[j]=distance[i]+cost[i][j];
+                        next[j]=i;
                     }
                 }
-        }while(flag);
+        }
+        for(i=1;i<=n;i++)
+                for(j=1;j<=n;j++){
+                    if(cost[i][j]!=999 && distance[j]>distance[i]+cost[i][j]){
+                        printf("Negative cycle exists\n");
+                        return;
+                    }
+                }
         printf("Shortest path from %d is given below\n",src);
         for(i=1;i<=n;i++)
             if(i!=src)
@@ -52,5 +57,5 @@ void main(){
                 }
             }
         printf("\n\n");
-    }
+    //}
 }
